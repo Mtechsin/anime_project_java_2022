327ff7858cf0dc2386a24ca826c8a568de32d74a
mtech <ahmedson3001@gmail.com>
2023-01-27 23:44:23 +0200

    making anime java project for collage in 2023

---
+++ src/anime/Anime.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package anime;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import java.awt.List;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+/**
+ *
+ * @author mtech
+ */
+public class Anime {
+
+    public static JFrame m;
+
+    /**
+     * @param args the command line arguments
+     *
+     */
+    
+  
+       
+   
+    public static void main(String[] args) throws UnirestException {
+        // TODO code application logic here
+         m = new mainview();
+        m.setLocationRelativeTo(null);
+        m.setVisible(true);
+        
+        /*
+                Connection c = new Connection();
+
+        System.out.println(c.first.get("duration"));
+        System.out.println(c.first.get("title"));
+        System.out.println(c.first.get("coverImage"));
+        System.out.println(c.first.get("bannerImage"));
+**/
+    }
+
+}
